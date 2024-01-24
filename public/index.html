6f1a27148b88ec8283a9835fe1f8f2048295ba15
Mathew Veale <mveale8@gmail.com>
2024-01-24 15:54:20 +1000

    Add React Setup

---
+++ public/index.html
@@ -0,0 +1,11 @@
+<!-- public/index.html -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Your React App</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is my first React app.</p>
+    </div>
+  );
+};
+
+export default App;
